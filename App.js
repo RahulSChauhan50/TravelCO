ce0e113bb6452eaf689972fea454ce191e5e8cc2
RahulSChauhan50 <rahulschauhan50@gmail.com>
2021-12-04 20:24:15 +0530

    added ReactNativePaper and react-native-size-matters

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './assets/screens/Home/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Provider as PaperProvider} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {createStackNavigator} from '@react-navigation/stack';
 const Stack = createStackNavigator();
@@ -50,33 +51,26 @@ function HomeScreen() {
         component={Home}
         //options={{headerShown: false}}
       />
-      <Tab.Screen
-        name="Orders"
-        component={Home}
-      />
-      <Tab.Screen
-        name="Report"
-        component={Home}
-      />
-      <Tab.Screen
-        name="Settlement"
-        component={Home}
-      />
+      <Tab.Screen name="Orders" component={Home} />
+      <Tab.Screen name="Report" component={Home} />
+      <Tab.Screen name="Settlement" component={Home} />
     </Tab.Navigator>
   );
 }
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen">
-        <Stack.Screen
-          name="HomeScreen"
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="HomeScreen">
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
